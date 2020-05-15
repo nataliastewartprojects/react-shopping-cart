74c9cf9ef3b09b6b1207b5a6ec302d8a87cb8ab0
Natalia <nataliamirandac@hotmail.com>
2020-05-15 10:14:25 -0400

    MPV done - passed cart as props through my Cartcontext to Navigation and SHoppingCart components

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
         <CartContext.Provider value={cart}>
-          <Navigation cart={cart} />
+          <Navigation />
 
           {/* Routes */}
 
@@ -31,7 +31,7 @@ function App() {
           </Route>
 
           <Route path="/cart">
-            <ShoppingCart cart={cart} />
+            <ShoppingCart />
           </Route>
         </CartContext.Provider>
       </ProductContext.Provider>
